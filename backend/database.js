7f67cdbc741876b0a558e035bdbff48659a62157
SamiDevW <SamiDevW@users.noreply.github.com>
2025-02-04 15:11:52 +0100

    user routes added

--- backend/database.js
+++ backend/database.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
+const connectDB = () => {
+	mongoose.connect("mongodb://localhost:27017/fourmis-chat");
 
-mongoose.connect("mongodb://localhost:27017/fourmis-chat", {});
+	const db = mongoose.connection;
 
-const db = mongoose.connection;
+	db.on("error", console.error.bind(console, "Erreur de connexion à MongoDB:"));
+	db.once("open", function () {
+		console.log("Connecté à MongoDB");
+	});
+}
 
-db.on("error", console.error.bind(console, "Erreur de connexion à MongoDB:"));
-db.once("open", function () {
-	console.log("Connecté à MongoDB");
-});
 
-module.exports = mongoose;
+module.exports = connectDB;
