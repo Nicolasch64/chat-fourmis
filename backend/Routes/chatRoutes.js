400abe5744083d2df6a9beac0bf31b211f8326bf
SamiDevW <SamiDevW@users.noreply.github.com>
2025-02-04 16:02:23 +0100

    Message Chat services added to routes

--- backend/Routes/chatRoutes.js
+++ backend/Routes/chatRoutes.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const router = express.Router()
+const Chat = require('../models/chat');
+
 
 router.get('/chat/:chat_id', async (req, res) => {
     const { chat_id } = req.params;
+    const chat = await findById(chat_id)
     console.log(chat_id);
-    res.send(chat_id)
+    res.send(chat)
 })
+router.post('/chat', async (req, res) => {
+    try {
+        const chat = new Chat()
+        await chat.save()
+        res.send(`chat ${chat} created`)
 
+    } catch (error) {
+        console.error(error);
+
+    }
+
+})
 
 
 
--- backend/models/message.js
+++ backend/models/message.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema({
-	username: { type: String, required: true },
+	author: { type: String, required: true },
 	content: { type: String, required: true },
-	timestamp: { type: Date, default: Date.now },
+	timestamp: { type: Date, default: Date.now }
 });
 
 const Message = mongoose.model("Message", messageSchema);
